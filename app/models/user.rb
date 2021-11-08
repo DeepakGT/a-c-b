ac6b3b8b1938bb7c186ed8eb5431386748506131
Mayank Gandhi <mayank.g@sigmainfo.net>
2021-11-08 20:47:38 +0530

    added letter-opener for mail-preview in dev environment

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@
 class User < ActiveRecord::Base
   extend Devise::Models
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :rememberable, :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :validatable
   include DeviseTokenAuth::Concerns::User
 end
