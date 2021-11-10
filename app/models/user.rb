8e869dd85ba1de6b361a486617b7605032ae93ff
Mayank Gandhi <mayank.g@sigmainfo.net>
2021-11-10 20:33:57 +0530

    format response and validate associated record

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,27 @@ class User < ActiveRecord::Base
   has_one :user_role, dependent: :destroy
   has_one :role, through: :user_role
 
+  # Validation
+  validates_associated :role
+  validates_presence_of :role
+
+  # Delegates
+  delegate :name, to: :role, prefix: true
+
+  # format response
+  def as_json(options={})
+    super(options)
+    .select{|key| key.in?(['email', 'uid', 'first_name', 'last_name'])}
+    .merge({role: humanize_role_name})
+  end
+
+  private
+
+    # access actual role name, which is in database
+    def humanize_role_name
+      Role.names[self.role_name]
+    end
+
+  # end of private
+
 end
