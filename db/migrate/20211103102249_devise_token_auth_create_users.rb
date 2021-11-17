f979d2e35540f61c33f29bff04f11c7229957032
Mayank Gandhi <mayank.g@sigmainfo.net>
2021-11-17 19:37:33 +0530

    fixed rubocops issues

--- db/migrate/20211103102249_devise_token_auth_create_users.rb
+++ db/migrate/20211103102249_devise_token_auth_create_users.rb
@@ -68,10 +68,10 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.1]
       t.timestamps
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, %i[uid provider],     unique: true
+    add_index :users, :email, unique: true
+    add_index :users, %i[uid provider], unique: true
     add_index :users, :reset_password_token, unique: true
-    add_index :users, :confirmation_token,   unique: true
+    add_index :users, :confirmation_token, unique: true
     # add_index :users, :unlock_token,         unique: true
   end
 end
--- db/migrate/20211116125821_create_addresses.rb
+++ db/migrate/20211116125821_create_addresses.rb
@@ -1,9 +1,9 @@
 class CreateAddresses < ActiveRecord::Migration[6.1]
   def change
     create_table :addresses do |t|
-      t.string :line_1
-      t.string :line_2
-      t.string :line_3
+      t.string :line1
+      t.string :line2
+      t.string :line3
       t.string :zipcode
       t.string :city
       t.string :state
