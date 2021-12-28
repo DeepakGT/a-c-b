24216f2dca87edf1cebbba1ea405840eadafe81e
sigma-kmadhu <kavya.m@sigmainfo.net>
2021-12-28 15:05:42 +0530

    update the cors-policies

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -18,6 +18,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put], expose: ['access-token', 'client', 'uid','expiry','token-type']
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], expose: ['access-token', 'client', 'uid','expiry','token-type']
   end
 end
