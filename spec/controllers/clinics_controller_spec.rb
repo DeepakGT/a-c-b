cec938a03875bc1899dbbfffa3cdd84469a4d05f
sigma-kmadhu <kavya.m@sigmainfo.net>
2021-12-30 19:19:24 +0530

    test cases for clinic add api

--- spec/controllers/clinics_controller_spec.rb
+++ spec/controllers/clinics_controller_spec.rb
@@ -28,4 +28,25 @@ RSpec.describe ClinicsController, type: :controller do
       end
     end
   end
+
+  describe "POST #create" do
+    let!(:user) { create(:user, :with_role, role_name: 'aba_admin') }
+    let!(:auth_headers) { user.create_new_auth_token }
+    let!(:organization) {create(:organization, name: 'org3', admin_id: user.id)}
+
+    context "when sign in" do
+      let(:clinic_name){'Test-clinic-1'}
+      it "should create a clinic" do
+        set_auth_headers(auth_headers)
+
+        post :create, params: {organization_id: organization.id, name: clinic_name}
+        response_body = JSON.parse(response.body)
+
+        expect(response.status).to eq(200)
+        expect(response_body['status']).to eq('success')
+        expect(response_body['data']['name']).to eq(clinic_name)
+        expect(response_body['data']['organization_id']).to eq(organization.id)
+      end
+    end
+  end
 end
