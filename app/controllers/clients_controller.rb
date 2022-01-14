cd4c7c45f63dd6afeca6a01eab223ea8bce72617
sigma-kmadhu <kavya.m@sigmainfo.net>
2022-01-14 13:00:03 +0530

    created client create api and its specs

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -4,4 +4,20 @@ class ClientsController < ApplicationController
   def index
     @clients = Client.order(:first_name)
   end
+
+  def create
+    @client = Client.new(client_params)
+    @client.role = Role.client.first
+    @client.save
+  end
+
+  private
+
+  def client_params
+    params.permit(:first_name, :last_name, :status, :gender, :email, :dob, :password, :password_confirmation,
+                  :clinic_id, contacts_attributes: [:first_name, :last_name, address_attributes:
+                  %i[line1 line2 line3 zipcode city state country addressable_type addressable_id], 
+                  phone_number_attributes: %i[phone_type number]])
+  end
+
 end
--- spec/controllers/clients_controller_spec.rb
+++ spec/controllers/clients_controller_spec.rb
@@ -11,19 +11,52 @@ RSpec.describe ClientsController, type: :controller do
 
   let!(:user) { create(:user, :with_role, role_name: 'aba_admin', first_name: 'admin', last_name: 'user') }
   let!(:auth_headers) { user.create_new_auth_token }
+  let!(:organization) {create(:organization, name: 'org1', admin_id: user.id)}
+  let!(:clinic) {create(:clinic, name: 'clinic1', organization_id: organization.id)}
 
   describe "GET #index" do
     context "when sign in" do
-      let!(:clients) { create_list(:client, 4, :with_role)}
+      let!(:clients) { create_list(:client, 4, :with_role, clinic_id: clinic.id)}
       it "should list client successfully" do
         set_auth_headers(auth_headers)
 
-        get :index
+        get :index, params: {clinic_id: clinic.id}
         response_body = JSON.parse(response.body)
 
         expect(response.status).to eq(200)
         expect(response_body['status']).to eq('success')
         expect(response_body['data'].count).to eq(clients.count)
+      end
+    end
+  end
+
+  describe "POST #create" do
+    context "when sign in" do
+      it "should create a client successfully" do
+        set_auth_headers(auth_headers)
+
+        post :create, params: { 
+          clinic_id: clinic.id,
+          first_name: 'test',
+          last_name: 'client',
+          email: 'testcontact@gamil.com',
+          password: '123456',
+          password_confirmation: '123456',
+          contacts_attributes: [{
+            first_name: 'test', 
+            last_name: 'contact', 
+            address_attributes: { city: 'Indore'},
+            phone_number_attributes: { number: '9988776655'}
+          }]
+        }
+        response_body = JSON.parse(response.body)
+        
+        expect(response.status).to eq(200)
+        expect(response_body['status']).to eq('success')
+        expect(response_body['data']['first_name']).to eq('test')
+        expect(response_body['data']['contacts'][0]['last_name']).to eq('contact')
+        expect(response_body['data']['contacts'][0]['address']['city']).to eq('Indore')
+        expect(response_body['data']['contacts'][0]['phone_number']['number']).to eq('9988776655')
       end
     end
   end
