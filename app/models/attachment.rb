f628d15848a9b3d8e48bed7342472d13b7cf111e
sigma-kmadhu <kavya.m@sigmainfo.net>
2022-02-09 20:05:33 +0530

    config s3

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -5,7 +5,9 @@ class Attachment < ApplicationRecord
 
   has_one_attached :file
 
+  # order is important here
   after_initialize :set_file
+  after_initialize :set_storage
 
   enum category: {
     lmn: 'lmn',
@@ -13,14 +15,28 @@ class Attachment < ApplicationRecord
     dx_video: 'dx_video'
   }, _prefix: true
 
-  def set_file
-    return if self.base64.blank?
-    decoded_data = Base64.decode64(self.base64.split(',')[1])
-    self.file = { 
-      io: StringIO.new(decoded_data),
-      content_type: 'image/jpeg',
-      filename: 'image.jpg'
-    }
-  end
+  private
+
+    def set_file
+      return if self.base64.blank?
+
+      decoded_data = Base64.decode64(self.base64.split(',')[1])
+      self.file = {
+        io: StringIO.new(decoded_data),
+        content_type: 'image/jpeg',
+        filename: 'image.jpg'
+      }
+    end
+
+    def set_storage
+      # larger that 5mb file would be upload on s3
+      if file.blob.byte_size > 5_000_000
+        Rails.application.config.active_storage.service = :amazon
+      else
+        Rails.application.config.active_storage.service = :local
+      end
+    end
+
+  # end of private
 
 end
