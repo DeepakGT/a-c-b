cdcf4a6bf925273a296a19ba50ed2aba9e5d71fe
sigma-kmadhu <kavya.m@sigmainfo.net>
2022-02-17 20:47:37 +0530

    client meta data api

---
+++ app/controllers/client_meta_data_controller.rb
@@ -0,0 +1,23 @@
+class ClientMetaDataController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_client
+
+  def selectable_options
+    @selectable_options = get_selectable_options_data
+  end
+
+  private
+
+  def set_client
+    @client = Client.find(params[:client_id])
+  end
+
+  def get_selectable_options_data
+    client_enrollments = @client.client_enrollments.active.where.not(source_of_payment: 'self_pay')
+    staff = @client.clinic.staff
+    selectable_options = { services: Service.all,
+                           client_enrollments: client_enrollments,
+                           service_providers: staff.service_providers }
+  end
+  # end of private
+end
