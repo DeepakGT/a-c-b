b1945be98efc9e1633d3f630847103b1f15832ad
sigma-kmadhu <kavya.m@sigmainfo.net>
2022-02-22 17:23:13 +0530

    added config

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -64,16 +64,12 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'stage989800.abaconnectemr.com' }
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.sendgrid.net',
-    port:                 587,
-    authentication:       'plain',
-    user_name:            Rails.application.credentials[:smtp][:user_name],
-    password:             Rails.application.credentials[:smtp][:password]
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_options  = {
+    from:  "no-reply <from_test_user@codes.com>"
   }
 
-  # config.require_master_key = true
+  config.require_master_key = true
 end
