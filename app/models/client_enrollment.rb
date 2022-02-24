301fe481ef9c227df865a3eba0abeb65bfe5d3e2
sigma-kmadhu <kavya.m@sigmainfo.net>
2022-02-24 12:04:23 +0530

    updated relationship enum in client enrollment

--- app/models/client_enrollment.rb
+++ app/models/client_enrollment.rb
@@ -2,7 +2,7 @@ class ClientEnrollment < ApplicationRecord
   belongs_to :client
   belongs_to :funding_source, optional: true
 
-  enum relationship: { parent_or_guardian: 0, spouse_or_partner: 1 }, _prefix: true
+  enum relationship: { parent_or_guardian: 0, spouse_or_partner: 1, self: 2 }, _prefix: true
   enum source_of_payment: { self_pay: 0, insurance: 1, single_case_agreement: 2 }
 
   scope :active, ->{ where('terminated_on > ?',Time.now.to_date).or(where('terminated_on IS NULL')) }
