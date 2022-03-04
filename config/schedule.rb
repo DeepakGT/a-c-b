8fd467b55860615121bb258f2a25e4e524cb3e26
sigma-kmadhu <kavya.m@sigmainfo.net>
2022-03-04 16:30:29 +0530

    updated schedule.rb file

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,8 @@
 set :output, "log/cron.log"
 
-every 1.day, at: '2:45 pm' do
+every 1.day, at: '5:00 pm' do
   rake "run_job:update_user_status"
 end
-
 
 # every 1.minute do
 #   rake "run_job:update_user_status"
