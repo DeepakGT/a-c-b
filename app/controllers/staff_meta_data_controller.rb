e020b6e43f42518145447ea0e1086e2272ee7a1f
sigma-kmadhu <kavya.m@sigmainfo.net>
2022-03-30 19:02:24 +0530

    updated clients_list api

--- app/controllers/staff_meta_data_controller.rb
+++ app/controllers/staff_meta_data_controller.rb
@@ -2,9 +2,14 @@ class StaffMetaDataController < ApplicationController
   before_action :authenticate_user!
 
   def clients_list
-    if current_user.role_name=='bcba' || current_user.role_name=='rbt'
+    if current_user.role_name=='rbt'
       clinic_ids = current_user.staff_clinics.pluck(:clinic_id) 
       clients = Client.where(clinic_id: clinic_ids)
+      clients = filter_by_location(clients) if params[:default_location_id].present?
+      @clients = clients
+    elsif current_user.role_name=='bcba'
+      clinic_ids = current_user.staff_clinics.pluck(:clinic_id) 
+      clients = Client.where(clinic_id: clinic_ids).or(Client.where(bcba_id: current_user.id))
       clients = filter_by_location(clients) if params[:default_location_id].present?
       @clients = clients
     end
