ff3be09a1a2b44e3b49725af590b1ea094319b79
sigma-kmadhu <kavya.m@sigmainfo.net>
2022-04-27 18:39:32 +0530

    updated changes

--- app/operations/snowflake/seed_client_data_operation.rb
+++ app/operations/snowflake/seed_client_data_operation.rb
@@ -10,7 +10,6 @@ module Snowflake
       def seed_client_data(username, password)
         db = Snowflake::SetDatabaseAndWarehouseService.call(username, password)
         appointments = Snowflake::GetAppointmentAdminDataService.call(db)
-
         appointments.each do |appointment|
           appointment = appointment.with_indifferent_access
           client_name = appointment['clientname']&.split(',')&.each(&:strip!)
@@ -18,6 +17,9 @@ module Snowflake
           client.status = appointment['clientstatus']&.downcase
           clinic_name = appointment['clientofficename']&.downcase
           client.clinic_id = Clinic.where('lower(name) = ? OR lower(aka) = ?', clinic_name, clinic_name)&.first&.id
+          if client.clinic_id.blank?
+            client.clinic_id = Clinic.create(name: clinic_name, aka: 'default_snowflake_clinic', organization_id: Organization.first.id).id
+          end
           client.save(validate: false)
           address = client.addresses.find_or_initialize_by(city: appointment['clientcity'], zipcode: appointment['clientzip'], address_type: 'service_address')
           address.is_default = true
