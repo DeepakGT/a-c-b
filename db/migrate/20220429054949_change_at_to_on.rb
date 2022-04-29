dd2015c343c490ea9c1902a410de43ce08ba0eff
sigma-kmadhu <kavya.m@sigmainfo.net>
2022-04-29 17:33:55 +0530

    fixed bugs

--- db/migrate/20220429054949_change_at_to_on.rb
+++
@@ -1,8 +0,0 @@
-class ChangeAtToOn < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :staff_qualifications, :issued_at, :issued_on
-    rename_column :staff_qualifications, :expires_at, :expires_on
-    rename_column :users, :hired_at, :hired_on
-    rename_column :schedulings, :unrendered_reason, :unrendered_reasons
-  end
-end
