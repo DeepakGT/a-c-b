cb4b2a76f2ddca778133b37592baf75d5a6e33d0
sigma-kmadhu <kavya.m@sigmainfo.net>
2022-05-18 16:11:58 +0530

    updated changes

--- app/operations/catalyst/render_appointments_operation.rb
+++ app/operations/catalyst/render_appointments_operation.rb
@@ -10,10 +10,10 @@ module Catalyst
       def render_service
         scheduling_ids = CatalystData.all&.pluck(:system_scheduling_id)&.uniq
         if scheduling_ids.present?
-          schedules = Scheduling.where(id: scheduling_ids).where('date < ?', Time.current.to_date).unrendered_schedulings
+          schedules = Scheduling.where(id: scheduling_ids).completed_scheduling.unrendered_schedulings
           if schedules.any?
             schedules.each do |schedule|
-              if schedule.unrendered_reason.blank?
+              if schedule.unrendered_reason != ['units_does_not_match']
                 RenderAppointments::RenderScheduleOperation.call(schedule.id)
               end
             end
