47ae882fa5b5acd9be93e440929139ed86447ad5
Kunal Awasthi <kunal.awasthi@sigmainfo.net>
2022-05-24 18:02:52 +0530

    Commented Sync op call in worker

--- app/workers/s3_sync_worker.rb
+++ app/workers/s3_sync_worker.rb
@@ -6,7 +6,7 @@ class S3SyncWorker
   def perform
     puts "#{DateTime.current}"
     puts "S3SyncWorker is started"
-    S3Sync::SyncAllTablesOperation.call
+    # S3Sync::SyncAllTablesOperation.call
     puts "S3SyncWorker is completed"
     puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
   end
