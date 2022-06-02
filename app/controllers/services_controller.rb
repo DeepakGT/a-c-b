892b390490101fde696054d8f1bc46a8d6de1fe9
sigma-kmadhu <kavya.m@sigmainfo.net>
2022-06-02 15:52:20 +0530

    updated changes

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -29,8 +29,8 @@ class ServicesController < ApplicationController
   private
 
   def service_params
-    params.permit(:name, :status, :display_code, :is_service_provider_required,
-                  service_qualifications_attributes: :qualification_id, :is_unassigned_appointment_allowed)
+    params.permit(:name, :status, :display_code, :is_service_provider_required, :is_unassigned_appointment_allowed
+                  service_qualifications_attributes: :qualification_id)
   end
 
   def set_service
