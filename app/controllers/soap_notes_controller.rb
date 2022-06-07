b2e3f2283239bb3b40b256c8f936987646379614
sigma-kmadhu <kavya.m@sigmainfo.net>
2022-06-07 17:56:10 +0530

    update soap_notes controller

--- app/controllers/soap_notes_controller.rb
+++ app/controllers/soap_notes_controller.rb
@@ -17,7 +17,7 @@ class SoapNotesController < ApplicationController
       @soap_note.user = current_user
       @soap_note.creator_id = current_user.id
       @soap_note.client_id = @scheduling.client_enrollment_service.client_enrollment.client.id
-      @soap_note.add_time = params[:add_time].to_datetime
+      @soap_note.add_time = params[:add_time].to_datetime&.in_time_zone('Eastern Time (US & Canada)') + 4.hours
       @soap_note.save
       RenderAppointments::RenderBySoapNoteOperation.call(@soap_note.id) if @scheduling.date<Time.current.to_date
     end
@@ -27,7 +27,7 @@ class SoapNotesController < ApplicationController
     SoapNote.transaction do
       update_signature
       @soap_note.user = current_user
-      @soap_note.add_time = params[:add_time].to_datetime
+      @soap_note.add_time = params[:add_time].to_datetime&.in_time_zone('Eastern Time (US & Canada)') + 4.hours
       @soap_note.update(soap_note_params)
       RenderAppointments::RenderBySoapNoteOperation.call(@soap_note.id) if @scheduling.date<Time.current.to_date
     end
