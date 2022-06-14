3f73bb69971b0599c791c43bea8ca693b0718a6f
sigma-kmadhu <kavya.m@sigmainfo.net>
2022-06-14 16:27:49 +0530

    implemented multiple soap notes of same location case

--- app/operations/catalyst/render_appointments_operation.rb
+++ app/operations/catalyst/render_appointments_operation.rb
@@ -14,19 +14,19 @@ module Catalyst
           schedules = Scheduling.where(id: scheduling_ids).completed_scheduling.unrendered_schedulings
           if schedules.any?
             schedules.each do |schedule|
-              if schedule.unrendered_reason.include?('units_does_not_match') && schedule.catalyst_data_ids.blank?
-                schedule.unrendered_reason = []
-                schedule.save(validate: false)
-              else
-                schedule.catalyst_data_ids.each do |catalyst_id|
-                  catalyst_data = CatalystData.find_by(id: catalyst_id)
-                  if catalyst_data.present?
-                    CompareCatalystDataWithSystemData::CompareSyncedDataOperation.call(catalyst_data)
-                    RenderAppointments::RenderScheduleOperation.call(schedule.id) if !schedule.unrendered_reason.include?('units_does_not_match')
-                  end
-                end
-              end
-              if !schedule.unrendered_reason.include?('units_does_not_match')
+              # if schedule.unrendered_reason.include?('units_does_not_match') && schedule.catalyst_data_ids.blank?
+              #   schedule.unrendered_reason = []
+              #   schedule.save(validate: false)
+              # else
+              #   schedule.catalyst_data_ids.each do |catalyst_id|
+              #     catalyst_data = CatalystData.find_by(id: catalyst_id)
+              #     if catalyst_data.present?
+              #       CompareCatalystDataWithSystemData::CompareSyncedDataOperation.call(catalyst_data)
+              #       RenderAppointments::RenderScheduleOperation.call(schedule.id) if !schedule.unrendered_reason.include?('units_does_not_match')
+              #     end
+              #   end
+              # end
+              if !schedule.unrendered_reason.include?('units_does_not_match') && !schedule.unrendered_reason.include?('multiple_soap_notes_found') && !schedule.unrendered_reason.include?('multiple_soap_notes_of_different_locations_found')
                 RenderAppointments::RenderScheduleOperation.call(schedule.id)
                 if schedule.is_rendered.to_bool.true?
                   Loggers::Catalyst::SyncSoapNotesLoggerService.call(schedule.id, "Schedule has been rendered successfully.")
