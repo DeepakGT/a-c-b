d39d665e30e47f171c1594d6e71db3f1380d1e9b
sigma-kmadhu <kavya.m@sigmainfo.net>
2022-06-15 16:12:54 +0530

    sync only active client's catalyst patient id

--- app/operations/catalyst/sync_clients_operation.rb
+++ app/operations/catalyst/sync_clients_operation.rb
@@ -13,30 +13,32 @@ module Catalyst
 
         client_data_array.each_index do |i|
           client_data = client_data_array[i]
-          if client_data['firstName']=='Jayce' && client_data['lastName']=='Giles' && client_data['dateOfBirth']&.to_time&.strftime('%Y-%m-%d')=='2016-12-18'
-            client_data['lastName'] = 'Gilles'
-          elsif client_data['firstName']=='Maley' && client_data['lastName']=='Lin-Yilan Soucy' && client_data['dateOfBirth']&.to_time&.strftime('%Y-%m-%d')=='2002-09-02'
-            client_data['firstName'] = ' Malei'
-          elsif client_data['firstName']=='Finn' && client_data['lastName']=='Byrne' && client_data['dateOfBirth']&.to_time&.strftime('%Y-%m-%d')=='2009-07-20'
-            client_data['firstName'] = 'Finn '
-          end
-          client = Client.find_by(first_name: client_data['firstName']&.strip, last_name: client_data['lastName']&.strip, dob: client_data['dateOfBirth']&.to_time&.strftime('%Y-%m-%d'))
-          if client_data['firstName']=='Olivia ' && client_data['lastName']=='Medard'
-            client = Client.find_by(first_name: client_data['firstName'], last_name: client_data['lastName'], dob: client_data['dateOfBirth']&.to_time&.strftime('%Y-%m-%d'))
-          end
-          if client.present?
-            client.catalyst_patient_id = client_data['patientId']
-            client.save(validate: false)
-            Loggers::Catalyst::SyncStaffAndClientsLoggerService.call(i, "Client #{client_data['firstName']} #{client_data['lastName']} catalyst patient id is saved.")
-          # else
-          #   client = Client.new(first_name: client_data['firstName'], last_name: client_data['lastName'], dob: client_data['dateOfBirth'], catalyst_patient_id: client_data['patientId'])
-          #   client.status = 'inactive' if client_data['active'].to_bool.false?
-          #   client.bcba_id = Staff.find_by(catalyst_user_id: client_data['bcba'])&.id
-          #   client.clinic_id = Clinic.find_by(catalyst_clinic_id: client_data['siteId'])&.id
-          #   client.gender = client_data['gender']==2 ? 1 : 0
-          #   client.save(validate: false)
-          else
-            Loggers::Catalyst::SyncStaffAndClientsLoggerService.call(i, "Client #{client_data['firstName']} #{client_data['lastName']} not found.")
+          if client_data['active']==true
+            if client_data['firstName']=='Jayce' && client_data['lastName']=='Giles' && client_data['dateOfBirth']&.to_time&.strftime('%Y-%m-%d')=='2016-12-18'
+              client_data['lastName'] = 'Gilles'
+            elsif client_data['firstName']=='Maley' && client_data['lastName']=='Lin-Yilan Soucy' && client_data['dateOfBirth']&.to_time&.strftime('%Y-%m-%d')=='2002-09-02'
+              client_data['firstName'] = ' Malei'
+            elsif client_data['firstName']=='Finn' && client_data['lastName']=='Byrne' && client_data['dateOfBirth']&.to_time&.strftime('%Y-%m-%d')=='2009-07-20'
+              client_data['firstName'] = 'Finn '
+            end
+            client = Client.find_by(first_name: client_data['firstName']&.strip, last_name: client_data['lastName']&.strip, dob: client_data['dateOfBirth']&.to_time&.strftime('%Y-%m-%d'))
+            if client_data['firstName']=='Olivia ' && client_data['lastName']=='Medard'
+              client = Client.find_by(first_name: client_data['firstName'], last_name: client_data['lastName'], dob: client_data['dateOfBirth']&.to_time&.strftime('%Y-%m-%d'))
+            end
+            if client.present?
+              client.catalyst_patient_id = client_data['patientId']
+              client.save(validate: false)
+              Loggers::Catalyst::SyncStaffAndClientsLoggerService.call(i, "Client #{client_data['firstName']} #{client_data['lastName']} catalyst patient id is saved.")
+            # else
+            #   client = Client.new(first_name: client_data['firstName'], last_name: client_data['lastName'], dob: client_data['dateOfBirth'], catalyst_patient_id: client_data['patientId'])
+            #   client.status = 'inactive' if client_data['active'].to_bool.false?
+            #   client.bcba_id = Staff.find_by(catalyst_user_id: client_data['bcba'])&.id
+            #   client.clinic_id = Clinic.find_by(catalyst_clinic_id: client_data['siteId'])&.id
+            #   client.gender = client_data['gender']==2 ? 1 : 0
+            #   client.save(validate: false)
+            else
+              Loggers::Catalyst::SyncStaffAndClientsLoggerService.call(i, "Client #{client_data['firstName']} #{client_data['lastName']} not found.")
+            end
           end
         end
       end
