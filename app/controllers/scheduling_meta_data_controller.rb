6ebdcfbac31a087ca66433e5ad86ebc98d06f041
Kavya Madhu <kavya.m@sigmainfo.net>
2022-07-23 00:45:55 +0530

    Removed active clients filter in rbt and bcba apis

--- app/controllers/scheduling_meta_data_controller.rb
+++ app/controllers/scheduling_meta_data_controller.rb
@@ -27,7 +27,7 @@ class SchedulingMetaDataController < ApplicationController
     end
     past_schedules = past_schedules.select("schedulings.*, 'Schedule' AS type")
     @past_schedules = past_schedules
-    catalyst_data = CatalystData.select("catalyst_data.*,clients.id AS client_id, clients.first_name, clients.last_name,'CatalystData' AS type").joins("LEFT JOIN clients ON (clients.catalyst_patient_id = catalyst_data.catalyst_patient_id)").by_active_clients.after_live_date.post_30_may_catalyst_data.by_catalyst_user_id(current_user.id).where(is_deleted_from_connect: false).and((CatalystData.with_no_appointments)).uniq
+    catalyst_data = CatalystData.select("catalyst_data.*,clients.id AS client_id, clients.first_name, clients.last_name,'CatalystData' AS type").joins("LEFT JOIN clients ON (clients.catalyst_patient_id = catalyst_data.catalyst_patient_id)").after_live_date.post_30_may_catalyst_data.by_catalyst_user_id(current_user.id).where(is_deleted_from_connect: false).and((CatalystData.with_no_appointments)).uniq
     @action_items_array = past_schedules.uniq.concat(catalyst_data)
     @action_items_array = sort_action_items(@action_items_array)
 
@@ -53,7 +53,7 @@ class SchedulingMetaDataController < ApplicationController
     # change_requests = SchedulingChangeRequest.by_approval_status
     # @change_requests = change_requests.by_bcba_ids(current_user.id)
     #                                   .or(change_requests.by_staff_ids(current_user.id)).left_outer_joins(:scheduling)
-    catalyst_data = CatalystData.select("catalyst_data.*,clients.id AS client_id, clients.first_name, clients.last_name,'CatalystData' AS type").joins("LEFT JOIN clients ON (clients.catalyst_patient_id = catalyst_data.catalyst_patient_id)").by_active_clients.after_live_date.post_30_may_catalyst_data.by_catalyst_user_id(current_user.id).where(is_deleted_from_connect: false).and((CatalystData.with_no_appointments)).uniq
+    catalyst_data = CatalystData.select("catalyst_data.*,clients.id AS client_id, clients.first_name, clients.last_name,'CatalystData' AS type").joins("LEFT JOIN clients ON (clients.catalyst_patient_id = catalyst_data.catalyst_patient_id)").after_live_date.post_30_may_catalyst_data.by_catalyst_user_id(current_user.id).where(is_deleted_from_connect: false).and((CatalystData.with_no_appointments)).uniq
     @action_items_array = past_schedules.uniq.concat(catalyst_data)
     @action_items_array = sort_action_items(@action_items_array)
 
