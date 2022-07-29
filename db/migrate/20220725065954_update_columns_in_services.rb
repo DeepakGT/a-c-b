3c7dacf169b5a98248feca7fafca9c77f25c4a2c
sigma-kmadhu <kavya.m@sigmainfo.net>
2022-07-29 16:58:55 +0530

    [ABA-413] updated services controller

--- db/migrate/20220725065954_update_columns_in_services.rb
+++ db/migrate/20220725065954_update_columns_in_services.rb
@@ -3,7 +3,7 @@ class UpdateColumnsInServices < ActiveRecord::Migration[6.1]
     remove_column :services, :selected_non_billable_payors, :string
     remove_column :services, :payors_requiring_rendering_provider, :string
 
-    add_column :services, :selected_payors, :text, array: true, default: []
+    add_column :services, :selected_payors, :json, default: []
     add_column :services, :max_units, :float
   end
 end
