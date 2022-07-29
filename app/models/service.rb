248033e37c0d65a8801d57f75c49fa65de91d4cd
sigma-kmadhu <kavya.m@sigmainfo.net>
2022-07-29 11:40:44 +0530

    [ABA-413] added selected_non_early_service_id to services

--- app/models/service.rb
+++ app/models/service.rb
@@ -14,7 +14,6 @@ class Service < ApplicationRecord
   # Enums
   enum status: {active: 0, inactive: 1}
 
-  serialize :selected_non_early_services, Array
   serialize :selected_payors, Array
 
   scope :non_early_services, ->{where(is_early_code: false)}
