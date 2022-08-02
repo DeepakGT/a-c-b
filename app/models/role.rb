06d37cad75ae6ba111b5fa160e655df3e35ac0f0
sigma-kmadhu <kavya.m@sigmainfo.net>
2022-08-02 15:15:14 +0530

    [ABA-429] updated roles list to not show system administrator

--- app/models/role.rb
+++ app/models/role.rb
@@ -4,4 +4,7 @@ class Role < ApplicationRecord
 
   validates :name, presence: true
   validates :name, uniqueness: { case_sensitive: false }
+
+  scope :except_system_admin, ->{ where.not(name: 'system_administrator') }
+  scope :except_super_admin, ->{ where.not(name: 'super_admin') }
 end
