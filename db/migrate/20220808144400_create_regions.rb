f10b49d0e487f168b3faf31ecb66643eccd10db1
Carlos Mario Mesa <cmesa@abatechnology.com>
2022-08-08 12:16:26 -0500

    remove comments

--- db/migrate/20220808144400_create_regions.rb
+++ db/migrate/20220808144400_create_regions.rb
@@ -1,15 +1,12 @@
 class CreateRegions < ActiveRecord::Migration[6.1]
   def change
-    # create regions
     create_table :regions do |t|
       t.string :name
       t.timestamps
     end
 
-    # add column at organization
     add_column :organizations, :id_regions, :jsonb, default: []
 
-    # add relation clinic to region
     add_reference :clinics, :region, foreign_key: true
     change_column_null :clinics, :region_id, true
   end
