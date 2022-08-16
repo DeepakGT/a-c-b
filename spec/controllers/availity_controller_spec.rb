9e435da7649af99cebf13fc284ccfaea6c8b32f5
Trung Nguyen <tnguyen@abacenters.com>
2022-08-16 10:34:41 -0400

    ABA-435: Minor tweak

--- spec/controllers/availity_controller_spec.rb
+++ spec/controllers/availity_controller_spec.rb
@@ -1,34 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe AvailityController, type: :controller do
-  context "when the user provides a valid api token" do
-    it "allows the user to pass" do
-      token = Rails.application.credentials[:connect_access_token]
-      request.headers.merge!({ "Authorization": "Bearer #{token}" })
-      put :update_claim_statuses
-      expect(response.status).to eq(200)
-    end
-  end
-
-  context "when the user provides an invalid api token" do
-    it "does not allow the user to pass" do
+  context "when sending claim statuses request to Availity with an invalid token" do
+    it "should return forbidden error" do
       token = "invalid123abc"
       request.headers.merge!({ "Authorization": "Bearer #{token}" })
-      put :update_claim_statuses
+      put :update_claim_statuses, params: {
+        bucket: "unloadingsnf-dev",
+        source_file: "availity/collab-pending-availity.csv",
+        target_file: "availity/collab-availity-done.csv"
+      }
       expect(response.status).to eq(401)
     end
   end
 
-  context "when sending claim statuses request to Availity" do
-    it "should return the claim status" do
-      rows = [
-        %w[CLAIMNUMBER FIRSTNAME LASTNAME FROMDATE PAYOR CLAIMAMOUNT TODATE BIRTHDATE GENDERCODE ACCOUNTNUMBER CORP_TAXID CORP_NPI SUBSCFIRST SUBSCLAST MEMBERID AVAILITY_STATUS],
-        ["555555555", "PFirst", "PLast", "6/20/2022", "BLUE CROSS BLUE SHIELD OF FLORIDA", "1000", "6/20/2022", "10/10/2010", "1", "66666666", "777777777", "1568982494", "SFirst", "SLast", "AKA999999999", ""]
-      ]
-      missing_payerid_errors = []
-      claim_status_errors = []
-      Availity::ProcessClaimsOperation.process_claims(rows, missing_payerid_errors, claim_status_errors)
-      expect(rows[1].last).not_to be nil
+  context "when sending claim statuses request to Availity with a valid token" do
+    it "should return http success" do
+      token = Rails.application.credentials[:connect_access_token]
+      request.headers.merge!({ "Authorization": "Bearer #{token}" })
+      put :update_claim_statuses, params: {
+        bucket: "unloadingsnf-dev",
+        source_file: "availity/collab-pending-availity.csv",
+        target_file: "availity/collab-availity-done.csv"
+      }
+      expect(response.status).to eq(200)
     end
   end
 end
