924b600e6d47d6ccf8f33995911e3cbc3d0355c7
johan <jechavarria@abatechnology.com>
2022-09-08 10:13:50 -0500

    ABA-635 added notification mail sending process
    created template
    created test

--- app/notifications/user_notification.rb
+++ app/notifications/user_notification.rb
@@ -1,5 +1,4 @@
 class UserNotification < Noticed::Base
   deliver_by :database
-  #deliver_by :email, mailer: "ApplicationMailer", if: :email_notifications?
-
+  deliver_by :email, mailer: 'NotificationMailer'
 end
