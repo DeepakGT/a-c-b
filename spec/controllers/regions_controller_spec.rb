59a263fd77e883ddd85492c1c7976835d1d5f62e
Carlos Mario Mesa <cmesa@abatechnology.com>
2022-09-08 17:41:37 -0500

    fix for comment to nathaly

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
-require "support/render_views"
+require 'support/render_views'
 RSpec.describe RegionsController, type: :controller do
-  let!(:role) { create(:role, name: Constant.super_admin, permissions: ['regions_view', 'regions_update'])}
+  let!(:role) { create(:role, name: Constant.super_admin, permissions: ['regions_view', 'regions_update']) }
   let!(:user) { create(:user, :with_role, role_name: role.name) }
   let!(:auth_headers) { user.create_new_auth_token }
-  let!(:regions) { create_list(:region, 4)}
+  let!(:regions) { create_list(:region, 4) }
 
-  describe "GET #index" do
-    context "when sign in" do
-      it "expect fetch regions list successfully" do
+  describe 'GET #index' do
+    context 'when sign in' do
+      it 'expect fetch regions list successfully' do
         set_auth_headers(auth_headers)
         get :index, format: :json
         response_body = JSON.parse(response.body)
@@ -19,11 +19,11 @@ RSpec.describe RegionsController, type: :controller do
     end
   end
 
-  describe "POST #create" do 
-    context "when sign in" do
-      it "expect create an regions successfully" do
+  describe 'POST #create' do 
+    context 'when sign in' do
+      it 'expect create an regions successfully' do
         set_auth_headers(auth_headers)
-        post :create, format: :json, params: {name: 'test'}
+        post :create, format: :json, params: { name: 'test' }
         response_body = JSON.parse(response.body)
         expect(response.status).to eq(200)
         expect(response_body['status']).to eq('success')
@@ -31,13 +31,13 @@ RSpec.describe RegionsController, type: :controller do
     end
   end
 
-  describe "POST #update" do 
-    let!(:region) { create(:region)}
+  describe 'POST #update' do 
+    let!(:region) { create(:region) }
 
-    context "when sign in" do
-      it "expect update an regions successfully" do
+    context 'when sign in' do
+      it 'expect update an regions successfully' do
         set_auth_headers(auth_headers)
-        patch :update, format: :json, params: {id: region.id, name: 'test2'}
+        patch :update, format: :json, params: {id: region.id, name: 'test2' }
         response_body = JSON.parse(response.body)
         expect(response.status).to eq(200)
         expect(response_body['status']).to eq('success')
