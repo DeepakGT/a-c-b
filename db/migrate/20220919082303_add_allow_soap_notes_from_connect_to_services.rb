94ba5fd822f4daee3c2fbd2355ec9fcb2e64e8c2
Kavya Madhu <kavya.m@sigmainfo.net>
2022-09-23 18:07:53 +0530

    [ABA-718], Updating services with display code 96112 and 96113 falgs to true

--- db/migrate/20220919082303_add_allow_soap_notes_from_connect_to_services.rb
+++ db/migrate/20220919082303_add_allow_soap_notes_from_connect_to_services.rb
@@ -1,5 +1,6 @@
 class AddAllowSoapNotesFromConnectToServices < ActiveRecord::Migration[6.1]
   def change
     add_column :services, :allow_soap_notes_from_connect, :boolean, default: false
+    ActiveRecord::Base.connection.execute "UPDATE services SET allow_soap_notes_from_connect = true where display_code IN ('96112', '96113');"  
   end
 end
